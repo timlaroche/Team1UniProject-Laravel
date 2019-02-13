9769c334b95564a12fa7fe180e7b5e939b0e4c59
Tim La Roche <timlaroche@users.noreply.github.com>
2019-02-13 14:25:11 +0000

    Logout works

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,4 +36,9 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function logout(Request $request) {
+      Auth::logout();
+      return redirect('/login');
+  }
 }
